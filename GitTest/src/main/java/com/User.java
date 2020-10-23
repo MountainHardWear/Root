3b175eca6ece949c395132b21779a3313a83e669
MountainHardWear <1971424964@qq.com>
2020-10-23 11:55:32 +0800

    第一次

---
+++ GitTest/src/main/java/com/User.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author : 李康军
+ * @date : 2020/10/23 11:54
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+    }
+}
