169e69faaa4127e9b81e785d80d67cacf1932f4d
MountainHardWear <1971424964@qq.com>
2020-10-23 11:58:20 +0800

    Test第一次

---
+++ GitTest/src/main/java/com/Test.java
@@ -0,0 +1,13 @@
+package com;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author : 李康军
+ * @date : 2020/10/23 11:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("嘤嘤嘤·");
+    }
+}
