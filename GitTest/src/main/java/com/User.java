684b27fae75ea837df1e7dd8d3a095ece7dd30c5
MountainHardWear <1971424964@qq.com>
2020-10-23 12:15:17 +0800

    第9

--- GitTest/src/main/java/com/User.java
+++ GitTest/src/main/java/com/User.java
@@ -8,5 +8,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("大家好");
         System.out.println("6666");
+        System.out.println("肖凯");
     }
 }
